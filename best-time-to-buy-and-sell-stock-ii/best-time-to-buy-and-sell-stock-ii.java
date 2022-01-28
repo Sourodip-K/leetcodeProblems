4225029f854172b822aa9e9aa9e72cd49b5ba229
Sourodip Kundu <sourodipkundu.w@gmail.com>
2022-01-28 23:56:58 +0530

    Time: 1 ms, Memory: 44.4 MB - LeetHub

---
+++ best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -0,0 +1,12 @@
+import java.util.Arrays; 
+class Solution {
+    public int maxProfit(int[] prices) {
+         int ans = 0;
+        int n=prices.length;
+        for (int i = 1; i < n; i++) {
+            if (prices[i] > prices[i - 1])
+                ans += prices[i] - prices[i - 1];
+        }
+        return ans;
+    }
+}
